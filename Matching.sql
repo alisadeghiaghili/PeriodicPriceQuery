0474092c3615dfefcab9bb80d096dd8f6b45760e
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2022-07-26 14:24:48 +0430

    Non-CTE version

--- Matching.sql
+++ Matching.sql
@@ -1,7 +1,36 @@
-Use Auction_DM;
-Go
-with Matches As (
 Select 
+	   tarikh as N'تاریخ'
+	 , nameTejarieKala As N'نام تجاری کالا'
+	 , nameTolidKonande As 'نام تولید کننده'
+	 , namaad As N'نماد'
+	 , noeQarardad As N'نوع قرارداد'
+	 , ArzeBeTon As N'عرضه (تن)'
+	 , vahedAsliArze As N'واحد اصلی عرضه'
+	 , zaribeTabdil As N'ضریب تبدیل'
+	 , vahedeVazn As N'واحد وزن'
+	 , qeymatePayeyeArze As N'قیمت پایه عرضه'
+	 , qeymateArzeDarTalar As N'قیمت عرضه در تالار'
+	 , taqazayeVoroodi As N'تقاضای ورودی - پایان سبز (تن)'
+	 , balatarinQeymateTaqaza As N'بالاترین قیمت تقاضا (ریال)'
+	 , sum(meqdareMoameleShode) As N'مقدار معامله شده'
+	 , kamtarinQeymateMoameleShode As N'کمترین قیمت معامله شده(ریال)'
+	 , qeymat As N'قیمت (ريال)'
+	 , motevaseteQeymateMoamele As N'متوسط قیمت معامله شده (واحد اصلی)'
+	 , bishtarinQeymat As N'بیشترین قیمت (ریال)'
+	 , sum(arzesheKol) As N'ارزش کل'
+	 , tarikheTahvil As N'تاریخ تحویل'
+	 , zirGorooh As N'زیرگروه کالا'
+	 , gorooh As N'گروه کالا'
+	 , gorooheAsli As N'گروه اصلی کالا'
+	 , arzeKonandeh As N'عرضه کننده'
+	 , ravesheArzeh As N'روش عرضه'
+	 , codeArzeh As N'کد عرضه'
+	 , noeArzeh As 'نوع عرضه'
+	 , nahveyeArzeh As 'نحوه عرضه'
+	 , shiveyeKharid As N'شیوه خرید'
+	 , tarikheQarardad As N'تاریخ قرارداد'
+from (
+	   Select 
 	   Date.PersianDate As tarikh
 	 , Symbol.Commodity_PersianName AS nameTejarieKala
 	 , Symbol.Producer_PersianName As nameTolidKonande
@@ -59,7 +88,7 @@ Inner Join Auction_DM.General_Dim.Date As ContractDate
 
 where Contract.IsExcess = 1
 	--And Contract.OfferItem_OriginalPK = 546517
-	And Contract.HallMatchingDate_ID = (select ID from Auction_DM.General_Dim.Date where PersianDate = '1401/03/18')
+	And Contract.HallMatchingDate_ID Between (select ID from Auction_DM.General_Dim.Date where PersianDate = '1401/03/18') And (select ID from Auction_DM.General_Dim.Date where PersianDate = '1401/03/18')
 	--And Date.PersianDate = '1401/03/18'
 
 group by
@@ -85,43 +114,11 @@ group by
 	   , SupplyMode.bSupplyModeDesc 
 	   , BuyMethod.PersianName
 	   , CustomerContract.Quantity
-	   , ContractDate.PersianDate)
+	   , ContractDate.PersianDate
+	   ) As Matching
 
-select tarikh as N'تاریخ'
-	 , nameTejarieKala As N'نام تجاری کالا'
-	 , nameTolidKonande As 'نام تولید کننده'
-	 , namaad As N'نماد'
-	 , noeQarardad As N'نوع قرارداد'
-	 , ArzeBeTon As N'عرضه (تن)'
-	 , vahedAsliArze As N'واحد اصلی عرضه'
-	 , zaribeTabdil As N'ضریب تبدیل'
-	 , vahedeVazn As N'واحد وزن'
-	 , qeymatePayeyeArze As N'قیمت پایه عرضه'
-	 , qeymateArzeDarTalar As N'قیمت عرضه در تالار'
-	 , taqazayeVoroodi As N'تقاضای ورودی - پایان سبز (تن)'
-	 , balatarinQeymateTaqaza As N'بالاترین قیمت تقاضا (ریال)'
-	 , sum(meqdareMoameleShode) As N'مقدار معامله شده'
-	 , kamtarinQeymateMoameleShode As N'کمترین قیمت معامله شده(ریال)'
-	 , qeymat As N'قیمت (ريال)'
-	 , motevaseteQeymateMoamele As N'متوسط قیمت معامله شده (واحد اصلی)'
-	 , bishtarinQeymat As N'بیشترین قیمت (ریال)'
-	 , sum(arzesheKol) As N'ارزش کل'
-	 , tarikheTahvil As N'تاریخ تحویل'
-	 , zirGorooh As N'زیرگروه کالا'
-	 , gorooh As N'گروه کالا'
-	 , gorooheAsli As N'گروه اصلی کالا'
-	 , arzeKonandeh As N'عرضه کننده'
-	 , ravesheArzeh As N'روش عرضه'
-	 , codeArzeh As N'کد عرضه'
-	 , noeArzeh As 'نوع عرضه'
-	 , nahveyeArzeh As 'نحوه عرضه'
-	 , shiveyeKharid As N'شیوه خرید'
-	 , tarikheQarardad As N'تاریخ قرارداد'
-from Matches
-
---where codeArzeh = 545206
-
-group by tarikh 
+group by 
+	   tarikh 
 	 , nameTejarieKala
 	 , nameTolidKonande
 	 , namaad 
